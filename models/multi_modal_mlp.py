2b69fdebb1dc818ef207943f038775c8b65c2fc4
Christopher Charlesworth <christopher.m.charlesworth@gmail.com>
2025-03-19 11:04:50 +0100

    multi modal mlp

---
+++ models/multi_modal_mlp.py
@@ -0,0 +1,19 @@
+from torchvision import models
+import torch.nn as nn
+import torch as torch
+
+class MultiModalMLP(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(MultiModalMLP, self).__init__()
+        self.fc1 = nn.Linear(input_size, 64)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(64, 32)
+        self.fc3 = nn.Linear(32, num_classes)
+        self.dropout = nn.Dropout(0.3)
+
+    def forward(self, x):
+        x = self.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
